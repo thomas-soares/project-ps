1b6d9d584b487e56b639eeecad20ded315b6536f
Thomas Soares <thomas__soares@hotmail.com>
2025-10-22 22:54:32 -0300

    feat: Enhance AppointmentCard component to accept appointment data as props

--- src/components/appointment-card/appointment-card.tsx
+++ src/components/appointment-card/appointment-card.tsx
@@ -1,4 +1,6 @@
-export const AppointmentCard = () => {
+type AppointmentCardProps = { appointment: Appointment };
+
+export const AppointmentCard = ({ appointment }: AppointmentCardProps) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-4">
       <h2 className="text-lg font-bold">Appointment Card</h2>
