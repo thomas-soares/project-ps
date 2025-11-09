d03437984d933830c6968bd25345f19a3b8260b4
Thomas Soares <thomas__soares@hotmail.com>
2025-11-08 23:34:03 -0300

    chore: Enhance AppointmentForm component to include time slots for scheduling

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,3 +1,10 @@
 export const AppointmentForm = () => {
-  return <h2>data de agendamento</h2>;
+  return (
+    <>
+      <h2>data de agendamento</h2>
+      <h2>Manhã</h2>
+      <h2>Tarde</h2>
+      <h2>Noite</h2>
+    </>
+  );
 };
