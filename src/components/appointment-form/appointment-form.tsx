9ea8ade36e6414084af1bc08b10cface32879847
Thomas Soares <thomas__soares@hotmail.com>
2025-11-11 23:56:23 -0300

    chore: Add text label to AppointmentForm component for better clarity

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -4,6 +4,7 @@ export const AppointmentForm = () => {
       <h2>data de agendamento</h2>
       <h2>Manhã</h2>
       <h2>Tarde</h2>
+      <div className="text-left">Horário</div>
     </>
   );
 };
