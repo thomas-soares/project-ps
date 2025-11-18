9a5708e7663329b8fa9e14d86839c56c96f5d174
Thomas Soares <thomas__soares@hotmail.com>
2025-11-17 23:57:01 -0300

    chore: Add time and patient labels to AppointmentForm for better organization

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -3,7 +3,11 @@ export const AppointmentForm = () => {
     <>
       <h2>data de agendamento</h2>
       <h2>Manhã</h2>
+      <div className="text-left">Horário</div>
+      <div className="text-left">Paciente</div>
       <h2>Tarde</h2>
+      <div className="text-left">Horário</div>
+      <div className="text-left">Paciente</div>
       <h2>Noite</h2>
       <div className="text-left">Horário</div>
       <div className="text-left">Paciente</div>
